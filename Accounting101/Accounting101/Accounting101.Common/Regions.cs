157a20cad2d4a3f5dda95370cf944f849987a68d
Michael Jordan <jordan.mymail@gmail.com>
2021-11-12 20:48:18 -0500

    Experimenting with regions for UI

--- Accounting101/Accounting101/Accounting101.Common/Regions.cs
+++ Accounting101/Accounting101/Accounting101.Common/Regions.cs
@@ -6,5 +6,6 @@ namespace Accounting101.Common
         public static string MainWindow => "MainWindow";
         public static string Documents => "Documents";
         public static string Navigation => "Navigation";
+        public static string ClientAddress => "ClientAddress";
     }
 }
---
+++ Accounting101/Accounting101/Accounting101.Modules/ViewModels/UsAddressViewModel.cs
@@ -0,0 +1,8 @@
+﻿using DevExpress.Mvvm;
+
+namespace Accounting101.Modules.ViewModels
+{
+    public class UsAddressViewModel : ViewModelBase
+    {
+    }
+}
