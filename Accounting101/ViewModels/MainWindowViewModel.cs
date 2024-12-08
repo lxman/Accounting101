3c26729982efa99567904fabda051c35eabf4efd
Michael Jordan <jordan.mymail@gmail.com>
2024-12-07 21:18:31 -0500

    Isolate menu to its own view model

--- Accounting101/ViewModels/MainWindowViewModel.cs
+++ Accounting101/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,7 @@
-﻿using System.Windows;
-using System.Windows.Input;
-using Accounting101.Commands;
+﻿using Accounting101.Commands;
 using Accounting101.Views.Create;
 using Accounting101.Views.List;
 using DataAccess;
-using DataAccess.Models;
-using DataAccess.Services;
 using DataAccess.Services.Interfaces;
 using Microsoft.VisualStudio.Threading;
 
@@ -16,32 +12,14 @@ namespace Accounting101.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        public MenuViewModel MenuViewModel { get; }
+
         public object PageContent
         {
             get => _pageContent;
             set => SetField(ref _pageContent, value);
         }
 
-        public ICommand NewCommand { get; }
-
-        public bool ShowSaveCommand
-        {
-            get => _showSaveCommand;
-            set => SetField(ref _showSaveCommand, value);
-        }
-
-        public ICommand SaveCommand { get; }
-
-        public bool ShowNewCommand
-        {
-            get => _showNewCommand;
-            set => SetField(ref _showNewCommand, value);
-        }
-
-        public ICommand ExitCommand { get; }
-
-        private bool _showNewCommand;
-        private bool _showSaveCommand;
         private readonly IDataStore _dataStore;
         private object _pageContent;
         private readonly JoinableTaskFactory _taskFactory;
@@ -49,26 +27,21 @@ namespace Accounting101.ViewModels
         public MainWindowViewModel(IDataStore dataStore, JoinableTaskFactory taskFactory)
         {
             _dataStore = dataStore;
-            _dataStore.StoreChanged += StoreChanged;
             _taskFactory = taskFactory;
-            ExitCommand = new DelegateCommand(() =>
-            {
-                _dataStore.Dispose();
-                Application.Current.Shutdown();
-            });
-            if (!taskFactory.Run(BusinessCreatedAsync))
+            MenuViewModel = new MenuViewModel(dataStore, taskFactory);
+            if (!taskFactory.Run(BusinessExistsAsync))
             {
                 CreateBusinessView createBusinessView = new(_dataStore, taskFactory);
                 CreateBusinessViewModel createBusinessViewModel = (CreateBusinessViewModel)createBusinessView.DataContext;
                 PageContent = createBusinessView;
-                SaveCommand = new DelegateCommand(() => BusinessViewSave(createBusinessViewModel));
+                MenuViewModel.SaveCommand = new DelegateCommand(() => BusinessViewSave(createBusinessViewModel));
             }
-            if (!taskFactory.Run(ClientsExistAsync))
+            if (!taskFactory.Run(ClientExistsAsync))
             {
                 CreateClientView createClientView = new(_dataStore, taskFactory);
                 CreateClientViewModel createClientViewModel = (CreateClientViewModel)createClientView.DataContext;
                 PageContent = createClientView;
-                SaveCommand = new DelegateCommand(() => ClientViewSave(createClientViewModel));
+                MenuViewModel.SaveCommand = new DelegateCommand(() => ClientViewSave(createClientViewModel));
             }
 
             ClientListView clientListView = new(_dataStore, taskFactory);
@@ -77,65 +50,6 @@ namespace Accounting101.ViewModels
                 ClientChosen(id);
             };
             PageContent = clientListView;
-        }
-
-        private static void StoreChanged(object? sender, ChangeEventArgs e)
-        {
-            if (e.ChangeType == ChangeType.Created)
-            {
-                if (e.ChangedType == typeof(Business))
-                {
-                }
-                else if (e.ChangedType == typeof(Client))
-                {
-                }
-
-                else if (e.ChangedType == typeof(Clients))
-                {
-
-                }
-                else if (e.ChangedType == typeof(Account))
-                {
-                }
-
-                else if (e.ChangedType == typeof(Accounts))
-                {
-
-                }
-                else if (e.ChangedType == typeof(Transaction))
-                {
-                }
-                else if (e.ChangedType == typeof(Transactions))
-                {
-                }
-            }
-            else
-            {
-                if (e.ChangedType == typeof(Transactions))
-                {
-
-                }
-
-                if (e.ChangedType == typeof(Transaction))
-                {
-
-                }
-                if (e.ChangedType == typeof(Accounts))
-                {
-                }
-                if (e.ChangedType == typeof(Account))
-                {
-                }
-                if (e.ChangedType == typeof(Clients))
-                {
-                }
-                if (e.ChangedType == typeof(Client))
-                {
-                }
-                if (e.ChangedType == typeof(Business))
-                {
-                }
-            }
         }
 
         private void BusinessViewSave(CreateBusinessViewModel m)
@@ -153,12 +67,12 @@ namespace Accounting101.ViewModels
             PageContent = new ClientAccountsView(_dataStore, _taskFactory, id);
         }
 
-        private async Task<bool> BusinessCreatedAsync()
+        private async Task<bool> BusinessExistsAsync()
         {
             return (await _dataStore.GetBusinessAsync()) is not null;
         }
 
-        private async Task<bool> ClientsExistAsync()
+        private async Task<bool> ClientExistsAsync()
         {
             return (await _dataStore.AllClientsAsync())?.Any() ?? false;
         }
---
+++ Accounting101/ViewModels/MenuViewModel.cs
@@ -0,0 +1,144 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Accounting101.Commands;
+using DataAccess;
+using DataAccess.Models;
+using DataAccess.Services;
+using DataAccess.Services.Interfaces;
+using Microsoft.VisualStudio.Threading;
+
+namespace Accounting101.ViewModels
+{
+    public class MenuViewModel : BaseViewModel
+    {
+        public ICommand NewCommand { get; }
+
+        public bool ShowNewCommand
+        {
+            get => _showNewCommand;
+            set => SetField(ref _showNewCommand, value);
+        }
+
+        public ICommand NewBusinessCommand { get; }
+
+        public bool ShowNewBusinessCommand
+        {
+            get => _showNewBusinessCommand;
+            set => SetField(ref _showNewBusinessCommand, value);
+        }
+
+        public ICommand NewClientCommand { get; }
+
+        public bool ShowNewClientCommand
+        {
+            get => _showNewClientCommand;
+            set => SetField(ref _showNewClientCommand, value);
+        }
+
+        public ICommand NewAccountCommand { get; }
+
+        public bool ShowNewAccountCommand
+        {
+            get => _showNewAccountCommand;
+            set => SetField(ref _showNewAccountCommand, value);
+        }
+
+        public ICommand NewTransactionCommand { get; }
+
+        public bool ShowNewTransactionCommand
+        {
+            get => _showNewTransactionCommand;
+            set => SetField(ref _showNewTransactionCommand, value);
+        }
+
+        public ICommand SaveCommand { get; set; }
+
+        public bool ShowSaveCommand
+        {
+            get => _showSaveCommand;
+            set => SetField(ref _showSaveCommand, value);
+        }
+
+        public ICommand ExitCommand { get; }
+
+        private bool _showNewCommand = true;
+        private bool _showNewBusinessCommand;
+        private bool _showNewClientCommand;
+        private bool _showNewAccountCommand;
+        private bool _showNewTransactionCommand;
+        private bool _showSaveCommand;
+        private readonly IDataStore _dataStore;
+        private readonly JoinableTaskFactory _taskFactory;
+
+        public MenuViewModel(IDataStore dataStore, JoinableTaskFactory taskFactory)
+        {
+            _dataStore = dataStore;
+            _dataStore.StoreChanged += StoreChanged;
+            _taskFactory = taskFactory;
+            ExitCommand = new DelegateCommand(() =>
+            {
+                _dataStore.Dispose();
+                Application.Current.Shutdown();
+            });
+        }
+
+        private static void StoreChanged(object? sender, ChangeEventArgs e)
+        {
+            if (e.ChangeType == ChangeType.Created)
+            {
+                if (e.ChangedType == typeof(Business))
+                {
+                }
+                else if (e.ChangedType == typeof(Client))
+                {
+                }
+
+                else if (e.ChangedType == typeof(Clients))
+                {
+
+                }
+                else if (e.ChangedType == typeof(Account))
+                {
+                }
+
+                else if (e.ChangedType == typeof(Accounts))
+                {
+
+                }
+                else if (e.ChangedType == typeof(Transaction))
+                {
+                }
+                else if (e.ChangedType == typeof(Transactions))
+                {
+                }
+            }
+            else
+            {
+                if (e.ChangedType == typeof(Transactions))
+                {
+
+                }
+
+                if (e.ChangedType == typeof(Transaction))
+                {
+
+                }
+                if (e.ChangedType == typeof(Accounts))
+                {
+                }
+                if (e.ChangedType == typeof(Account))
+                {
+                }
+                if (e.ChangedType == typeof(Clients))
+                {
+                }
+                if (e.ChangedType == typeof(Client))
+                {
+                }
+                if (e.ChangedType == typeof(Business))
+                {
+                }
+            }
+        }
+    }
+}
